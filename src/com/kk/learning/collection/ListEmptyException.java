146999d0648804d45ef4129eb433dc1673317ca6
KiranCS <cskirankumar@gmail.com>
2016-02-04 17:59:34 +0530

    ListEmptyExeption changes

--- src/com/kk/learning/collection/ListEmptyException.java
+++ src/com/kk/learning/collection/ListEmptyException.java
@@ -2,9 +2,6 @@ package com.kk.learning.collection;
 
 public class ListEmptyException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public ListEmptyException(){
